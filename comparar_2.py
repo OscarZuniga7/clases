83ff3da0096366bf6d4ca3346eb731f903f84703
Oscar <oscarzuniga7@gmail.com>
2024-08-17 01:19:25 +0000

    comparar completo

---
+++ comparar_2.py
@@ -0,0 +1,9 @@
+x = int(input("¿Cuál es el valor de x? "))
+y = int(input("¿Cuál es el valor de y? "))
+
+if x < y:
+    print("x es menor que y")
+if x > y:
+    print("x es mayor que y")
+if x == y:
+    print("x es igual a y")
