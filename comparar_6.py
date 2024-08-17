2c7f8b99ac64113d687bc31583cb15c15d5c937c
Oscar <oscarzuniga7@gmail.com>
2024-08-17 01:37:09 +0000

    perfeccionar or

--- comparar_6.py
+++ comparar_6.py
@@ -1,7 +1,7 @@
 x = int(input("¿Cuál es el valor de x? "))
 y = int(input("¿Cuál es el valor de y? "))
 
-if x != y :
-    print("x no es igual a y")
-else:
+if x == y :
     print("x es igual a y")
+else:
+    print("x no es igual a y")
