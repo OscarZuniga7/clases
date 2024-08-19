a94783ca4a8a0f0f6c04ae31e9d97987361a1ea8
Oscar <oscarzuniga7@gmail.com>
2024-08-19 23:51:55 +0000

    condicionales

--- comparar_5.py
+++ comparar_5.py
@@ -1,7 +1,7 @@
 x = int(input("¿Cuál es el valor de x? "))
 y = int(input("¿Cuál es el valor de y? "))
 
-if x != y :
+if x == y :
     print("x no es igual a y")
 else:
     print("x es igual a y")
